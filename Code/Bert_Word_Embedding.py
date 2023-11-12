38bd5d4f8b012ee38ec4f5edf3ed82c2eda05f03
realonepointfive <131595883+realonepointfive@users.noreply.github.com>
2023-11-12 16:28:17 +1100

    Add files via upload

--- Code/Bert_Word_Embedding.py
+++ Code/Bert_Word_Embedding.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 from transformers import BertTokenizer
+import torch
 
 curr_dir = os.path.dirname(__file__)
 rela_dir = "../Dataset/Flood_input_data/"
@@ -12,13 +13,50 @@ sentences = df.sentence.values
 print('Loading BERT tokenizer...')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
-# Print the original sentence.
-print(' Original: ', sentences[0])
+# For every sentence...
+for sent in sentences:
 
-# Print the sentence split into tokens.
-print('Tokenized: ', tokenizer.tokenize(sentences[0]))
+    # Tokenize the text and add `[CLS]` and `[SEP]` tokens.
+    input_ids = tokenizer.encode(sent, add_special_tokens=True)
 
-# Print the sentence mapped to token ids.
-print('Token IDs: ', tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sentences[0])))
+    # Update the maximum sentence length.
+    max_len = max(max_len, len(input_ids))
 
+print('Max sentence length: ', max_len)
 
+# Tokenize all of the sentences and map the tokens to thier word IDs.
+input_ids = []
+attention_masks = []
+
+# For every sentence...
+for sent in sentences:
+    # `encode_plus` will:
+    #   (1) Tokenize the sentence.
+    #   (2) Prepend the `[CLS]` token to the start.
+    #   (3) Append the `[SEP]` token to the end.
+    #   (4) Map tokens to their IDs.
+    #   (5) Pad or truncate the sentence to `max_length`
+    #   (6) Create attention masks for [PAD] tokens.
+    encoded_dict = tokenizer.encode_plus(
+                        sent,                      # Sentence to encode.
+                        add_special_tokens = True, # Add '[CLS]' and '[SEP]'
+                        max_length = max_len,           # Pad & truncate all sentences.
+                        pad_to_max_length = True,
+                        return_attention_mask = True,   # Construct attn. masks.
+                        return_tensors = 'pt',     # Return pytorch tensors.
+                   )
+    
+    # Add the encoded sentence to the list.    
+    input_ids.append(encoded_dict['input_ids'])
+    
+    # And its attention mask (simply differentiates padding from non-padding).
+    attention_masks.append(encoded_dict['attention_mask'])
+
+# Convert the lists into tensors.
+input_ids = torch.cat(input_ids, dim=0)
+attention_masks = torch.cat(attention_masks, dim=0)
+
+# Print sentence 0, now as a list of IDs.
+print('Original: ', sentences[0])
+print('Token IDs:', input_ids[0])
+print('Attention_masks', attention_masks[0])
